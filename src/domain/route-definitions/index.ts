1ea61df79a5bd766bdc860c510d1d5d333b2a481
ClassicalMoser <classicalmoser@gmail.com>
2026-06-20 20:19:09 -0500

    fix: formatting

--- src/domain/route-definitions/index.ts
+++ src/domain/route-definitions/index.ts
@@ -1,6 +1,10 @@
 export type { Route } from './route';
 export type { GetRoute, GetRouteValidators } from './get-route';
-export type { PostRoute, CreatedPostRoute, PostRouteValidators } from './post-route';
+export type {
+  PostRoute,
+  CreatedPostRoute,
+  PostRouteValidators,
+} from './post-route';
 export type { PutRoute, PutRouteValidators } from './put-route';
 export type { PatchRoute, PatchRouteValidators } from './patch-route';
 export type { DeleteRoute, DeleteRouteValidators } from './delete-route';
